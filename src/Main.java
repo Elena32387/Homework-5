84f83464172d9260b40bf6719c5b3edf67994075
Elena32387 <iea20ster@gmail.com>
2024-11-26 20:54:46 +0300

    Домашняя работа 5

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,106 @@
+import java.sql.Array;
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Задание 1 и 2");
 
-    }
-}
+        int[] arrOne = new int[3];
+        arrOne[0] = 1;
+        arrOne[1] = 2;
+        arrOne[2] = 3;
+        System.out.println(arrOne[0]);
+        System.out.println(arrOne[1]);
+        System.out.println(arrOne[2]);
+
+        double[] arrTwo = {1.57, 7.654, 9.986};
+        System.out.println(arrTwo[0]);
+        System.out.println(arrTwo[1]);
+        System.out.println(arrTwo[2]);
+
+        byte[] arrThree = {-77, 2, 100};
+        System.out.println(arrThree[0]);
+        System.out.println(arrThree[1]);
+        System.out.println(arrThree[2]);
+
+        System.out.println("Задание 2");
+
+        int[] firstArray = new int[]{1, 2, 3};
+        for (int i = 0; i < firstArray.length; i++) {
+            System.out.print(firstArray[i]);
+            if (i != firstArray.length - 1) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println();
+
+
+        double[] secondArray = {1.57, 7.654, 9.986};
+        for (int i = 0; i < secondArray.length; i++) {
+            System.out.print(secondArray[i]);
+            if (i != secondArray.length - 1) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println();
+
+
+        byte[] thirdArray = {-77, 2, 100};
+        for (int i = 0; i < thirdArray.length; i++) {
+            System.out.print(thirdArray[i]);
+            if (i != thirdArray.length - 1) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println();
+
+        System.out.println("Задание 3");
+
+        for (int i = firstArray.length - 1; i >= 0; i--) {
+            System.out.print(firstArray[i]);
+            if (i != 0) {
+                System.out.print(" , ");
+            }
+        }
+
+        System.out.println();
+
+
+        for (int i = secondArray.length - 1; i >= 0; i--) {
+            System.out.print(secondArray[i]);
+            if (i != 0) {
+                System.out.print(" , ");
+            }
+        }
+
+        System.out.println();
+
+        for (int i = thirdArray.length - 1; i >= 0; i--) {
+            System.out.print(thirdArray[i]);
+            if (i != 0) {
+                System.out.print(" , ");
+            }
+        }
+        System.out.println();
+
+        System.out.println("Задание 4");
+
+        for (int i = 0; i<firstArray.length; i++){
+            if (firstArray[i] % 2 != 0){
+                firstArray[i]++;}}
+        System.out.println(Arrays.toString(firstArray));
+            }
+        }
+
+
+
+
+
+
+
+
